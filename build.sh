14a2e9024c3e09a06f4ebfd7367919f0a7fff97a
guybrush <realguybrush@vivaldi.net>
2023-06-30 14:18:27 +0700

    automatically run tests after build

--- build.sh
+++ build.sh
@@ -4,9 +4,16 @@
 TARGET=8
 
 # compile and build jar
+echo "BUILDING"
 [ -d target ] && rm -r target
 mkdir -p target/META-INF/services
 echo "jamaica.fluent.fluent" > target/META-INF/services/com.sun.source.util.Plugin
 javac -source $TARGET -target $TARGET -d target fluent.java
 cd target; jar --create --file fluent.jar *
+cd ..
+
+# test
+echo "TESTING"
+javac -cp target/fluent.jar -Xplugin:fluent test.java
+java test
 
