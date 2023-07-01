9177d38a1d529a7b2ed4503d91efaa293117850f
guybrush <realguybrush@vivaldi.net>
2023-07-02 00:06:02 +0700

    add mvn installation step

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+VERSION=0.1.0
+
 # location of jdk for building fluent
 [ ! "$JAVA_HOME" ] && JAVA_HOME="$(dirname $(dirname $(readlink -f $(which javac))))"
 
 # directories containing jdks to test against, separated by spaces
-JDKS="$HOME/tools/jdk-*"
+#JDKS="$HOME/tools/jdk-*"
 [ ! "$JDKS" ] && JDKS="$JAVA_HOME"
 
 # target java 8 so we can import com.sun.tools.javac.*
@@ -20,7 +22,7 @@ $JAVA_HOME/bin/javac -source $TARGET -target $TARGET -d target Fluent.java
 cd target; $JAVA_HOME/bin/jar --create --file ../fluent.jar *; cd ..
 
 # test against all jdks
-echo "===== TESTING ====="
+echo "\n===== TESTING ====="
 echo "----- press enter to being testing valid code"; read x
 for JDK in $JDKS; do
     echo $JDK
@@ -31,6 +33,10 @@ echo "\n----- press enter to begin testing code with errors"; read x
 for JDK in $JDKS; do
     echo $JDK
     "$JDK"/bin/javac -cp fluent.jar -Xplugin:fluent -d target testErrors.java
-    echo "----- press enter to continue"; read x
+    echo "\n----- press enter to continue"; read x
 done
+
+# install using maven
+echo "===== INSTALLING WITH MAVEN ====="
+mvn install:install-file -DgroupId=jamaica -DartifactId=fluent -Dversion=$VERSION -Dpackaging=jar -Dfile=fluent.jar
 
