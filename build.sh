9109d7b02fe2b5491b12d7824c92f2e6ad13335d
guybrush <realguybrush@vivaldi.net>
2023-07-01 18:39:14 +0700

    add support for testing on multiple jdks

--- build.sh
+++ build.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 
-# expose the compiler module by targetting java 8
+# location of jdk for building fluent
+[ ! "$JAVA_HOME" ] && JAVA_HOME="$(dirname $(dirname $(readlink -f $(which javac))))"
+
+# directories containing jdks to test against, separated by spaces
+JDKS="$HOME/tools/jdk-*"
+[ ! "$JDKS" ] && JDKS="$JAVA_HOME"
+
+# target java 8 so we can import com.sun.tools.javac.*
 TARGET=8
 
 # compile and build jar
-echo "BUILDING"
+echo "===== BUILDING ====="
 [ -d target ] && rm -r target
 mkdir -p target/META-INF/services
 echo "com.sun.tools.javac.comp.Fluent" > target/META-INF/services/com.sun.source.util.Plugin
-javac -source $TARGET -target $TARGET -d target Fluent.java
-cd target; jar --create --file ../fluent.jar *; cd ..
+$JAVA_HOME/bin/javac -source $TARGET -target $TARGET -d target Fluent.java
+cd target; $JAVA_HOME/bin/jar --create --file ../fluent.jar *; cd ..
 
-# test
-echo "TESTING"
-javac -cp fluent.jar -Xplugin:fluent -d target test.java
-java -cp target test
+# test against all jdks
+echo "===== TESTING ====="
+for JDK in $JDKS; do
+    echo $JDK
+    "$JDK"/bin/javac -cp fluent.jar -Xplugin:fluent -d target test.java
+    "$JDK"/bin/java -cp target test
+done
 
