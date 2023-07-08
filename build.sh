1432c19a2018fe901b425949b646ecc43e670dfb
guybrush <realguybrush@vivaldi.net>
2023-07-08 14:45:10 +0700

    parameterise build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+NAME=fluent
+CLASSNAME=Fluent
 VERSION=0.1.0
 TARGET=9
-JAR=fluent.jar
+JAR=$NAME.jar
 
-# location of jdk for building fluent
+# location of jdk for building
 [ ! "$JAVA_HOME" ] && JAVA_HOME="$(dirname $(dirname $(readlink -f $(which javac))))"
 
 # directories containing jdks to test against, separated by spaces
 JDKS="$JAVA_HOME"
-#JDKS="$HOME/tools/jdk-*"
+JDKS="$HOME/tools/jdk-*"
 
-# javac arguments to inject the compiled plugin
-WITH_PLUGINS="-Xplugin:fluent -J--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED"
+# javac arguments to invoke the compiled plugin
+WITH_PLUGINS="-Xplugin:$NAME -J--add-opens=java.base/jdk.internal.misc=ALL-UNNAMED"
+TEST_CLASSPATH=$JAR
 
 # compile and build jar
 # note: -source 8 is required to import com.sun.tools.javac.*
@@ -20,13 +23,12 @@ echo "===== BUILDING ====="
 echo $JAVA_HOME
 [ -d target ] && rm -r target
 mkdir -p target/META-INF/services
-echo "com.sun.tools.javac.comp.Fluent" > target/META-INF/services/com.sun.source.util.Plugin
-$JAVA_HOME/bin/javac -nowarn -source 8 -target $TARGET -d target Fluent.java
+echo "com.sun.tools.javac.comp.$CLASSNAME" > target/META-INF/services/com.sun.source.util.Plugin
+$JAVA_HOME/bin/javac -nowarn -source 8 -target $TARGET -d target $CLASSNAME.java
 [ $? -eq 0 ] || exit 1
 cd target; $JAVA_HOME/bin/jar --create --file ../$JAR *; cd ..
 
 # test against all jdks
-TEST_CLASSPATH=$JAR:../unchecked/unchecked.jar
 echo "\n===== TESTING ====="
 for JDK in $JDKS; do
     echo $JDK
@@ -44,5 +46,5 @@ done
 
 # install using maven
 echo "===== INSTALLING WITH MAVEN ====="
-mvn install:install-file -DgroupId=jamaica -DartifactId=fluent -Dversion=$VERSION -Dpackaging=jar -Dfile=$JAR
+mvn install:install-file -DgroupId=jamaica -DartifactId=$NAME -Dversion=$VERSION -Dpackaging=jar -Dfile=$JAR
 
